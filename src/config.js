13a3e27d155524807c4c677add98fe6cf0bcdb4a
litlle-pig <semenlegostaev@gmail.com>
2025-01-30 10:57:51 +0300

    new

---
+++ src/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCaQ8SR3_frZOBdfqW8h9D5b6u6_s5HC_E",
+  authDomain: "affordable-health-471c3.firebaseapp.com",
+  projectId: "affordable-health-471c3",
+  storageBucket: "affordable-health-471c3.firebasestorage.app",
+  messagingSenderId: "431180645019",
+  appId: "1:431180645019:web:a20927e810cf8491823c3e",
+  measurementId: "G-7B16Q7BD1Z",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
