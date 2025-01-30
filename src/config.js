8c2fed412c03788220869bc89b176e9dd2ec6fc9
litlle-pig <semenlegostaev@gmail.com>
2025-01-30 12:57:44 +0300

    base

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCaQ8SR3_frZOBdfqW8h9D5b6u6_s5HC_E",
   authDomain: "affordable-health-471c3.firebaseapp.com",
   projectId: "affordable-health-471c3",
@@ -15,7 +15,3 @@ const firebaseConfig = {
   appId: "1:431180645019:web:a20927e810cf8491823c3e",
   measurementId: "G-7B16Q7BD1Z",
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
